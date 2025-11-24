85cccef79df63a3f15fb6eecca0e5dc28e2c4f9a
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-24 11:38:40 +0100

    wip

--- rainbowrl/Agent.py
+++ rainbowrl/Agent.py
@@ -4,13 +4,16 @@ from .RainbowNetwork import RainbowNetwork
 
 
 class Agent:
-    def __init__(self, environment: str, lr: float = 0.0001):
+    def __init__(
+        self, environment: str, lr: float = 0.0001, training_starts: int = 80_000
+    ):
+        self.environment = environment
+        self.training_starts = training_starts
+
         self.network = RainbowNetwork()
         self.target = RainbowNetwork()
         self.target.load_state_dict(self.network.load_state_dict())
-
         self.optimizer = torch.optim.Adam(self.network.parameters(), lr=lr)
-        self.environment = environment
 
         self.t = 0
 
@@ -22,6 +25,9 @@ class Agent:
         self.t += 1
 
     def train(self):
+        if self.t < self.training_starts:
+            return
+
         self.optimizer.zero_grad()
         loss = self.loss()
         loss.backward()
