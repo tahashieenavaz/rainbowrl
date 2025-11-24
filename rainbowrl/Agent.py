4032cb49ee96c0b423afb407d6c864bbcad078a2
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-24 13:27:39 +0100

    adds buffer

--- rainbowrl/Agent.py
+++ rainbowrl/Agent.py
@@ -1,5 +1,6 @@
 import torch
 from .RainbowNetwork import RainbowNetwork
+from .PrioritizedReplayBuffer import PrioritizedReplayBuffer
 from typing import NewType
 
 LossValue = NewType("LossValue", float)
@@ -20,7 +21,11 @@ class Agent:
         initial_beta: float = 0.4,
         total_timesteps: int = 10_000_000,
         buffer_size: int = 1_000_00,
+        image_size: int = 84,
         alpha: float = 0.5,
+        steps: int = 3,
+        max_priority: float = 1.0,
+        epsilon: float = 1e-6,
     ):
         self.environment = environment
         self.alpha = alpha
@@ -39,12 +44,17 @@ class Agent:
         )
         self.target.load_state_dict(self.network.load_state_dict())
         self.optimizer = torch.optim.Adam(self.network.parameters(), lr=lr)
+        self.buffer = PrioritizedReplayBuffer(
+            buffer_size, image_size, steps, max_priority, alpha, epsilon
+        )
 
         self.t = 0
 
     @torch.no_grad()
     def action(self, state: torch.Tensor):
-        pass
+        q_dist = self.network(state)
+        q_values = torch.sum(q_dist * self.support, dim=2)
+        return torch.argmax(q_values, dim=1).cpu().numpy()
 
     def train(self) -> LossValue:
         if self.t < self.training_starts:
