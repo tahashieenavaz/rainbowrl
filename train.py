ed02632c0c99346cbcbd41e8d7b760c49253261d
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-24 16:52:26 +0100

    removes extra default settings ovveride

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from rainbowrl import Agent
 
-agent = Agent(environment="ALE/Pong-v5", training_starts=2000)
+agent = Agent(environment="ALE/Pong-v5")
 data = agent.loop(verbose=True)
 
 print(data.loss, data.hns, data.rewards)
